0c64d7015ba382d2c2219827ea160aa284ca704d
Shourya Bansal <shouryab2001@gmail.com>
2021-11-27 12:49:18 +0530

    third commit : create chat

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,50 @@
-import React from "react";
+import {
+  AttachFile,
+  Mic,
+  MoreVert,
+  SearchOutlined,
+  InsertEmoticon,
+} from "@mui/icons-material";
+import { Avatar, IconButton } from "@mui/material";
+import React, { useState } from "react";
+import "./Chat.css";
 
 function Chat(props) {
-  return <div></div>;
+  return (
+    <div className="chat">
+      <div className="chat_header">
+        <Avatar
+          src={`https://avatars.dicebear.com/api/avataaars/${7997}.svg`}
+        />
+
+        <div className="chat_headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at....</p>
+        </div>
+
+        <div className="chat_headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat_body"></div>
+      <div className="chat_footer">
+        <InsertEmoticon />
+        <form>
+          <input placeholder="Type a message" type="text" />
+          <button type="submit">Send a message</button>
+        </form>
+        <Mic />
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
