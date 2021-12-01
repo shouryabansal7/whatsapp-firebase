50901b0bb450f7bb054f0794ca95245410c1edbd
Shourya Bansal <shouryab2001@gmail.com>
2021-12-01 19:11:08 +0530

    eight commit : fetch messages from firebase and add last message functionality

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,12 @@
-import { collection, query, where, onSnapshot, doc } from "@firebase/firestore";
+import {
+  collection,
+  onSnapshot,
+  doc,
+  addDoc,
+  orderBy,
+  query,
+  QuerySnapshot,
+} from "@firebase/firestore";
 import {
   AttachFile,
   Mic,
@@ -11,11 +19,15 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import "./Chat.css";
 import db from "./firebase";
+import { serverTimestamp } from "@firebase/firestore";
+import { useStateValue } from "./StateProvider";
 
 function Chat(props) {
   const [input, setInput] = useState("");
   const { roomId } = useParams();
   const [roomName, setRoomName] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [{ user }, dispatch] = useStateValue();
 
   const [seed, setSeed] = useState("");
 
@@ -26,8 +38,46 @@ function Chat(props) {
   useEffect(() => {
     if (roomId) {
       onSnapshot(doc(db, "rooms", roomId), (doc) => {
+        console.log("doc 1", doc);
         setRoomName(doc.data().name);
       });
+
+      onSnapshot(doc(db, "rooms", roomId), (doc) => {
+        const q = query(
+          collection(doc.ref, "messages"),
+          orderBy("timestamp", "asc")
+        );
+        onSnapshot(q, (querySnapshot) => {
+          let messages = [];
+          querySnapshot.forEach((doc) => {
+            messages.push({
+              data: doc.data(),
+            });
+          });
+          setMessages(messages);
+        });
+      });
+
+      // onSnapshot(doc(db, "rooms", roomId), (doc) => {
+      //   console.log(doc.ref);
+      //   onSnapshot(
+      //     collection(doc.ref, "messages"),
+      //     orderBy("timestamp", "desc"),
+      //     (snapshot) => {
+      //       console.log("snapshot", snapshot);
+      //       let messages = [];
+      //       snapshot.docs.forEach((doc) => {
+      //         console.log("doc", doc.data());
+      //         messages.push({
+      //           data: doc.data(),
+      //         });
+      //       });
+      //       console.log("messages", messages);
+      //       console.log(user.displayName === "2K20_SE_131_Shourya Bansal");
+      //       setMessages(messages);
+      //     }
+      //   );
+      // });
     }
   }, [roomId]);
 
@@ -35,7 +85,16 @@ function Chat(props) {
     e.preventDefault();
     console.log("You typped>>>>", input);
 
-    setInput("");
+    onSnapshot(doc(db, "rooms", roomId), (doc) => {
+      console.log(doc.ref);
+      addDoc(collection(doc.ref, "messages"), {
+        message: input,
+        name: user.displayName,
+        timestamp: serverTimestamp(),
+      });
+
+      setInput("");
+    });
   };
   return (
     <div className="chat">
@@ -46,7 +105,12 @@ function Chat(props) {
 
         <div className="chat_headerInfo">
           <h3>{roomName}</h3>
-          <p>Last seen at....</p>
+          <p>
+            Last seen{" "}
+            {new Date(
+              messages[messages.length - 1]?.data.timestamp?.toDate()
+            ).toUTCString()}
+          </p>
         </div>
 
         <div className="chat_headerRight">
@@ -62,16 +126,20 @@ function Chat(props) {
         </div>
       </div>
       <div className="chat_body">
-        <p className="chat_message">
-          <span className="chat_name">Soumya</span>
-          Hey Guys..
-          <span className="chat_timestamp">3:52pm</span>
-        </p>
-        <p className={`chat_message ${true && "chat_receiver"}`}>
-          <span className="chat_name">Shourya</span>
-          Namaste
-          <span className="chat_timestamp">3:53pm</span>
-        </p>
+        {messages.map((message) => (
+          //console.log(message.data.message);
+          <p
+            className={`chat_message ${
+              message.data.name === user.displayName && "chat_receiver"
+            }`}
+          >
+            <span className="chat_name">{message.data.name}</span>
+            {message.data.message}
+            <span className="chat_timestamp">
+              {new Date(message.data.timestamp?.toDate()).toUTCString()}
+            </span>
+          </p>
+        ))}
       </div>
       <div className="chat_footer">
         <InsertEmoticon />
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,12 +1,60 @@
-import { addDoc, collection } from "@firebase/firestore";
 import { Avatar } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import db from "./firebase";
 import "./SidebarChat.css";
+import {
+  collection,
+  query,
+  where,
+  onSnapshot,
+  doc,
+  getDoc,
+  addDoc,
+  FieldValue,
+  orderBy,
+} from "@firebase/firestore";
 
 function SidebarChat(props) {
+  const { addNewChat, id, name } = props;
+  const [messages, setMessages] = useState([]);
   const [seed, setSeed] = useState("");
+
+  useEffect(() => {
+    if (id) {
+      onSnapshot(doc(db, "rooms", id), (doc) => {
+        const q = query(
+          collection(doc.ref, "messages"),
+          orderBy("timestamp", "desc")
+        );
+        onSnapshot(q, (querySnapshot) => {
+          let messages = [];
+          querySnapshot.forEach((doc) => {
+            messages.push({
+              data: doc.data(),
+            });
+          });
+          setMessages(messages);
+        });
+      });
+
+      // onSnapshot(doc(db, "rooms", id), (doc) => {
+      //   onSnapshot(
+      //     collection(doc.ref, "messages"),
+      //     orderBy("timestamp", "desc"),
+      //     (snapshot) => {
+      //       let messages = [];
+      //       snapshot.docs.forEach((doc) => {
+      //         messages.push({
+      //           data: doc.data(),
+      //         });
+      //       });
+      //       setMessages(messages);
+      //     }
+      //   );
+      // });
+    }
+  }, []);
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
@@ -22,7 +70,6 @@ function SidebarChat(props) {
     }
   };
 
-  const { addNewChat, id, name } = props;
   return !addNewChat ? (
     <Link to={`/rooms/${id}`}>
       <div className="sidebarChat">
@@ -31,7 +78,7 @@ function SidebarChat(props) {
         />
         <div className="sidebarChat_info">
           <h2>{name}</h2>
-          <p>This is the last message</p>
+          <p>{messages[0]?.data.message}</p>
         </div>
       </div>
     </Link>
